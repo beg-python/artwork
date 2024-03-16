01ab659f6e3ae989c0d82fe00b22989ff3c4e5bc
beg-python <163522707+beg-python@users.noreply.github.com>
2024-03-16 14:31:45 +0500

    Add files via upload

--- classes.py
+++ classes.py
@@ -48,7 +48,7 @@ class ArtworkItem:
     
     def display_info(self):
         print("--Artwork--")
-        print({self.__artwork.get_info()})
+        print(self.__artwork.get_info())
         print(f"Location: {self.__location}")
 
 class Exhibition:
@@ -89,14 +89,17 @@ class Exhibition:
     def remove_artwork(self, artwork_item):
         self.__artworks.remove(artwork_item) #removing artwork from list
     
-    def get_info(self):
+    def display_info(self):
         print("--Exhibition--")
         print("ID: ", self.__id)
         print("Name: ", self.__name)
         print("Start Date: ", self.__start_date)
         print("End Date: ", self.__end_date)
         print("Location: ", self.__location)
-        print("Artworks in Exhibition: ", self.__artworks)
+        print("Artworks in Exhibition: ", end=" ")
+        for artwork_item in self.__artworks:
+            print(artwork_item.get_artwork().get_title(), end="   ")
+        print() 
 
 class Visitor:
     def __init__(self, id, name, age, profession):
@@ -126,67 +129,106 @@ class Visitor:
         return 0
     
 class Adult(Visitor):
-    def apply_discount(self, original_price):
-        if 18 <= self.__age <= 60:
+    def apply_discount(self):
+        if 18 <= self.get_age() <= 60:
             return 0  #no discount
         else:
             return -1  #invalid age for adult
         
 class Others(Visitor):
-    def apply_discount(self, original_price):
-        if self.__age <18 or self.__age >60 or self.__profession == "student" or self.__profession == "teacher":
-            return 0.5
+    def apply_discount(self):
+        if self.get_age() <18 or self.get_age() >60 or self.get_profession() == "student" or self.get_profession() == "teacher":
+            return 1 #free ticket
+        else:
+            return 0 #no discount
 
 class Group:
-    def __init__(self, group_id, leader):
+    def __init__(self, group_id, guide):
         self.__group_id = group_id
-        self.__leader = leader
-        self.__members = [] #people in a group
+        self.__guide = guide
+        self.__members = [] #members in a group
+        if len(self.__members) < 15:
+            print()
+            print("--Error!!: Cannot create group with less than 15 members--")
+            print()
+        self.__max = 40
+        self.__min = 15
 
     def get_group_id(self):
         return self.__group_id
     def set_group_id(self, new_group_id):
         self.__group_id = new_group_id
-    def get_leader(self):
-        return self.__leader
-    def set_leader(self, new_leader):
-        self.__leader = new_leader
-    def get_members(self):
-        return self.__members
-    
+    def get_guide(self):
+        return self.__guide
+    def set_guide(self, new_guide):
+        self.__guide = new_guide
+
+    def display_members(self):
+        print("Group Members: " , end=" ")
+        for i in self.__members:
+            print(i.get_name(), end="     ")
+        print()
+
     def add_member(self, member):
-        self.__members.append(member)
+        if self.member_count() <= self.__max:
+            self.__members.append(member)
 
     def remove_member(self, member):
-        if member in self.__members:
+        if member in self.__members and self.member_count() >= self.__min:
             self.__members.remove(member)
         else:
             print(f"{member} is not a member of this group.")
     
     def member_count(self):
-        return len(self.__member) #count of mmebers
+        return len(self.__members) #count of mmebers
     
-    def apply_discount(self, original_price):
-        return 0.5  #50% discount 
     
-
-class Ticket:
-    def __init__(self, id, visitor_id, ticket_type, event_id, purchase_date, price, exhibition, visitor):          
+class Member:
+    def __init__(self, id, name, age, profession):
         self.__id = id
-        self.__ticket_type = ticket_type
-        self.__purchase_date = purchase_date
-        self.__price = price
-        self.__exhibition = exhibition
-        self.__visitor = visitor
+        self.__name = name
+        self.__age = age
+        self.__profession = profession
 
     def get_id(self):
         return self.__id
     def set_id(self, new_id):
         self.__id = new_id
-    def get_ticket_type(self):
-        return self.__ticket_type
-    def set_ticket_type(self, new_ticket_type):
-        self.__ticket_type = new_ticket_type
+    def get_name(self):
+        return self.__name
+    def set_name(self, new_name):
+        self.__name = new_name
+    def get_age(self):
+        return self.__age
+    def set_age(self, new_age):
+        self.__age = new_age
+    def get_profession(self):
+        return self.__profession
+    def set_profession(self, new_profession):
+        self.__profession = new_profession
+
+    def apply_discount(self):
+        return 0.5  #50% discount 
+
+
+class Ticket:
+    def __init__(self, id, type, purchase_date, price, exhibition, visitor, group_member):          
+        self.__id = id
+        self.__type = type
+        self.__purchase_date = purchase_date
+        self.__price = price
+        self.__exhibition = exhibition
+        self.__visitor = visitor
+        self.__group_member = group_member
+
+    def get_id(self):
+        return self.__id
+    def set_id(self, new_id):
+        self.__id = new_id
+    def get_type(self):
+        return self.__type
+    def set_type(self, new_type):
+        self.__type = new_type
     def get_purchase_date(self):
         return self.__purchase_date
     def set_purchase_date(self, new_purchase_date):
@@ -203,6 +245,10 @@ class Ticket:
         return self.__visitor
     def set_visitor(self, new_visitor):
         self.__visitor = new_visitor
+    def get_group_member(self):
+        return self.__group_member
+    def set_group_member(self, new_group_member):
+        self.__group_member = new_group_member
 
     def validate_ticket(self):
         current = date.today()
@@ -212,22 +258,41 @@ class Ticket:
             return False
         
     def calculate_price(self):
-        discount_percent = self.visitor.apply_discount(self.__price)
-        if discount_percent == -1:
-            return None  
+
+        if self.__visitor != None:
+            discount_percent = self.__visitor.apply_discount()
         else:
-            discounted_price = self.__price - (self.__price * discount_percent)
-            final_price = discounted_price * 1.05  #5% VAT
-            return final_price
-        
-    def ticket_info(self):
+            discount_percent = self.__group_member.apply_discount()
+
+        if discount_percent == -1:
+            return None
+        elif isinstance(self.__group_member, Member):  #group member
+            discount_percent = self.__group_member.apply_discount()  
+        elif isinstance(self.__visitor, Visitor):  #visitor
+            discount_percent = self.__visitor.apply_discount()
+
+        discounted_price = self.__price - (self.__price * discount_percent)
+        final_price = discounted_price * 1.05  # 5% VAT
+
+        return final_price
+    
+    def display_info(self):
         print("--Ticket--")
         print("ID: ",self.__id)
-        print("Ticket Type: ", self.__ticket_type)
         print("Purchase Date: ", self.__purchase_date)
         print("Price: ", self.__price)
-        print("Visitor ID: ", self.__visitor.get_id()) #visitor ID
-        print("Location: ", self.__exhibition.get_location()) #exhibition location
+
+        if self.__visitor != None:
+            print("Visitor Name: ",self.__visitor.get_name())
+            print("Visitor ID: ", self.__visitor.get_id()) 
+        if self.__group_member != None:
+            print("Group Member Name: ", self.__group_member.get_name())          
+            print("Group Member ID: ", self.__group_member.get_id()) 
+
+        print("Exhibition Location: ", self.__exhibition.get_location()) #exhibition location
+        print("Exhibition Name: ", self.__exhibition.get_name()) #name of exhibition
+        print("Exhibition Duration: ", self.__exhibition.get_duration()) #time of exhibition
+
 
 
 
